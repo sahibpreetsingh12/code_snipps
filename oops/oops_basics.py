de3b55127fc520921196c0d02efe1a0d8297dea3
sahibpreet singh <ss9334931@gmail.com>
2019-04-13 10:01:21 +0530

    Add files via upload

---
+++ oops/oops_basics.py
@@ -0,0 +1,16 @@
+class Employee:
+
+    def __init__(self, first, last, pay):#this method will take instance of the class as it's first variable 
+        self.first = first          #f or emp_1 it is acting as emp_1.first=first
+                                    
+        self.last = last            
+        
+        self.email = first + '.' + last + '@email.com'
+        
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('sahib', 'singh', 50000)
+emp_2 = Employee('berry', 'james', 60000)
